b41438e97fe8bcc36327499a21f907fecf569c4d
Harrison Smith <harrison@hsmith.dev>
2025-05-10 19:25:29 -0600

    Add project files.

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,9 @@
+// Harrison Smith
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    cout << "Git is now tracking this project!" << endl;
+    return 0;
+}
---
+++ Number Guessing Game/Number Guessing Game.cpp
@@ -0,0 +1,28 @@
+// Number Guessing Game.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+/*
+*   Notes: The program is a simple number guessing game that has 3 levels of difficulty.
+*   The user can choose a difficulty level, and the program will generate a random number within a specific range.
+*   The user has to guess the number, and the program will provide feedback on whether the guess is too high or too low.
+*   The user has a limited number of attempts to guess the number correctly.
+*   The program will keep track of the number of attempts and display the number of attempts taken to guess the number correctly.
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
