fc13871e13ddb4cfd117726a72a59ef505a48852
Harrison Smith <harrison@hsmith.dev>
2025-05-29 14:11:41 -0600

    cpp files are not needed from templates, moved required code to the header file

--- Fractions/fraction.h
+++ Fractions/fraction.h
@@ -1,43 +1,94 @@
 #pragma once
+#include <iostream>
+#include <stdexcept>
 
 namespace mathlib {
-	template <typename T>
-	class Fraction {
-	private:
-		T numerator;
-		T denomerator;
+    template <typename T>
+    class Fraction {
+    private:
+        T numerator;
+        T denominator;
 
-	public:
-		// Constructor
-		mathlib::Fraction(T num, T denum);
+    public:
+        // Constructors
+        Fraction() : numerator(0), denominator(1) {}
+        Fraction(T num, T denum) {
+            if (denum == 0) {
+                throw std::invalid_argument("Denominator cannot be zero.");
+            }
+            numerator = num;
+            denominator = denum;
+            simplify();
+        }
 
-		// Arthmetic Operators Overloading
-		Fraction<T> operator + (const mathlib::Fraction<T>& other) const;
-		Fraction<T> operator - (const mathlib::Fraction<T>& other) const;
-		Fraction<T> operator * (const mathlib::Fraction<T>& other) const;
-		Fraction<T> operator / (const mathlib::Fraction<T>& other) const;
+        // Arithmetic Operators
+        Fraction<T> operator+(const Fraction<T>& other) const {
+            return Fraction(numerator * other.denominator + other.numerator * denominator, denominator * other.denominator);
+        }
 
-		// Comparison Operators Overloading
-		bool operator == (const mathlib::Fraction<T>& other) const;
-		bool operator != (const mathlib::Fraction<T>& other) const;
-		bool operator < (const mathlib::Fraction<T>& other) const;
-		bool operator > (const mathlib::Fraction<T>& other) const;
-		bool operator <= (const mathlib::Fraction<T>& other) const;
-		bool operator >= (const mathlib::Fraction<T>& other) const;
+        Fraction<T> operator-(const Fraction<T>& other) const {
+            return Fraction(numerator * other.denominator - other.numerator * denominator, denominator * other.denominator);
+        }
 
-		// Stream Operators Overloading
-		template <typename U>
-		friend std::ostream& operator<<(std::ostream& os, const Fraction<U>& frac);
-		template <typename U>
-		friend std::istream& operator>>(std::istream& is, Fraction<U>& frac);
+        Fraction<T> operator*(const Fraction<T>& other) const {
+            return Fraction(numerator * other.numerator, denominator * other.denominator);
+        }
 
-		// Helper Functions
-		void simplify();
-		double toDouble() const;
-		int gcd(T numerator, T denominator);
+        Fraction<T> operator/(const Fraction<T>& other) const {
+            if (other.numerator == 0) {
+                throw std::invalid_argument("Cannot divide by zero.");
+            }
+            return Fraction(numerator * other.denominator, denominator * other.numerator);
+        }
 
-		// Get Functions
-		T getNumerator() { return numerator; }
-		T getDenominator() { return denomerator; }
-	};
+        // Comparison Operators
+        bool operator==(const Fraction<T>& other) const {
+            return numerator * other.denominator == other.numerator * denominator;
+        }
+
+        bool operator!=(const Fraction<T>& other) const { return !(*this == other); }
+        bool operator<(const Fraction<T>& other) const { return numerator * other.denominator < other.numerator * denominator; }
+        bool operator>(const Fraction<T>& other) const { return !(*this <= other); }
+        bool operator<=(const Fraction<T>& other) const { return *this < other || *this == other; }
+        bool operator>=(const Fraction<T>& other) const { return !(*this < other); }
+
+        // Stream Operators
+        template <typename U>
+        friend std::ostream& operator<<(std::ostream& os, const Fraction<U>& frac) {
+            os << frac.numerator << '/' << frac.denominator;
+            return os;
+        }
+
+        template <typename U>
+        friend std::istream& operator>>(std::istream& is, Fraction<U>& frac) {
+            char separator;
+            is >> frac.numerator >> separator >> frac.denominator;
+            if (frac.denominator == 0) {
+                throw std::invalid_argument("Denominator cannot be zero.");
+            }
+            frac.simplify();
+            return is;
+        }
+
+        // Helper Functions
+        void simplify() {
+            T gcd = Fraction::gcd(numerator, denominator);
+            numerator /= gcd;
+            denominator /= gcd;
+            if (denominator < 0) {
+                numerator = -numerator;
+                denominator = -denominator;
+            }
+        }
+
+        static T gcd(T a, T b) {
+            return (b == 0) ? a : gcd(b, a % b);
+        }
+
+        double toDouble() const { return static_cast<double>(numerator) / denominator; }
+
+        // Getters
+        T getNumerator() const { return numerator; }
+        T getDenominator() const { return denominator; }
+    };
 }
